bbba5aaeb007c71942deaafce2b7869f6a22646a
Aaron Harris <aaronah02@icloud.com>
2025-11-06 02:38:21 -0400

    fix(Button): correct color naming for secondary variant

--- packages/react/src/components/Button/Button.tsx
+++ packages/react/src/components/Button/Button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
         primary:
           'bg-teal-dark text-neutral-white hover:bg-brand-teal-light hover:shadow-[inset_0_0_0_4px_rgba(222,245,246,0.10)] active:bg-brand-teal-darker active:shadow-[inset_0_0_0_3px_rgba(0,0,0,0.20)] active:outline-none active:ring-4 active:ring-teal-100 active:ring-offset-1 active:rounded-sm focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-teal-100 focus-visible:ring-offset-1 focus-visible:rounded-sm',
         secondary:
-          'bg-neutral-gray-light text-neutral-black hover:shadow-[inset_0_0_0_4px_rgba(0,0,0,0.05)] active:bg-brand-neutral-gray-dark active:shadow-[inset_0_0_0_3px_rgba(0,0,0,0.10)] active:outline-none active:ring-4 active:ring-teal-100 active:ring-offset-1 active:rounded-sm focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-teal-100 focus-visible:ring-offset-1 focus-visible:rounded-sm',
+          'bg-neutral-grey text-neutral-black hover:shadow-[inset_0_0_0_4px_rgba(0,0,0,0.05)] active:bg-brand-neutral-gray-dark active:shadow-[inset_0_0_0_3px_rgba(0,0,0,0.10)] active:outline-none active:ring-4 active:ring-teal-100 active:ring-offset-1 active:rounded-sm focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-teal-100 focus-visible:ring-offset-1 focus-visible:rounded-sm',
       },
       size: {
         default: 'px-6 py-4',
